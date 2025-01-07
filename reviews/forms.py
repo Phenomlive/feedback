81a71088a137e48339986b013f102d0561595815
Fuad <fuadreddhow@gmail.com>
2025-01-07 20:26:55 +0600

    implemented crud operations

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,18 @@
 from django import forms
+from . models import Review
 
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(max_length=100, label='Your Name')
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(max_length=100, label='Your Name')
+#     review_text = forms.CharField(widget=forms.Textarea, label='Your Feedback', max_length=500)
+#     rating = forms.IntegerField(min_value=1, max_value=5, label='Your Rating')
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        labels = {
+            'user_name': 'Your Name',
+            'review_text': 'Your Feedback',
+            'rating': 'Your Rating',
+        }
--- reviews/views.py
+++ reviews/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
+from .models import Review
+from django.views import View
 
 # Create your views here.
 
-def review(request):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            return HttpResponseRedirect('/thank-you')
-    else:
+class ReviewView(View):
+    def get(self, request):
         form = ReviewForm()
 
-    return render(request, 'reviews/review.html', {
-        'form': form,
-    })
+        return render(request, 'reviews/review.html', {
+            'form': form,
+        })
+    
+    def post(self, request):
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thank-you')
+        
+        return render(request, 'reviews/review.html', {
+            'form': form,
+        })
+
 
 def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
+    user_name = Review.objects.latest('id').user_name
+    return render(request, 'reviews/thank_you.html', {
+        'user_name': user_name
+    })
